5cfb42ff8cdb9a1dfb79bf86fdb0fcf3eb5e3044
Josh Girvin <josh@jgirvin.com>
2025-09-15 17:08:17 +1000

    Basic resolver wired up to test

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,4 +1,66 @@
-import fastify, { FastifyReply, FastifyRequest } from 'fastify'
-import { createYoga } from 'graphql-yoga'
+import fastify, { type FastifyReply, type FastifyRequest } from 'fastify'
+import {
+  createSchema,
+  createYoga,
+  useExecutionCancellation,
+} from 'graphql-yoga'
 
 const app = fastify({ logger: true })
+
+const yoga = createYoga<{
+  req: FastifyRequest
+  reply: FastifyReply
+}>({
+  plugins: [useExecutionCancellation()],
+  schema: createSchema({
+    typeDefs: /* graphql */ `
+      scalar File
+
+      interface Node {
+        id: ID!
+      }
+
+      type Query {
+        hello: String
+      }
+    `,
+    resolvers: {
+      Query: {
+        hello() {
+          return 'world'
+        },
+      },
+    },
+  }),
+
+  // Integrate Fastify logger
+  logging: {
+    debug: (...args) => args.forEach(arg => app.log.debug(arg)),
+    info: (...args) => args.forEach(arg => app.log.info(arg)),
+    warn: (...args) => args.forEach(arg => app.log.warn(arg)),
+    error: (...args) => args.forEach(arg => app.log.error(arg)),
+  },
+})
+
+/**
+ * We pass the incoming HTTP request to GraphQL Yoga
+ * and handle the response using Fastify's `reply` API
+ * Learn more about `reply` https://www.fastify.io/docs/latest/Reply/
+ **/
+app.route({
+  // Bind to the Yoga's endpoint to avoid rendering on any path
+  url: yoga.graphqlEndpoint,
+  method: ['GET', 'POST', 'OPTIONS'],
+  handler: (req, reply) =>
+    yoga.handleNodeRequestAndResponse(req, reply, {
+      req,
+      reply,
+    }),
+})
+
+// This will allow Fastify to forward multipart requests to GraphQL Yoga
+app.addContentTypeParser('multipart/form-data', {}, (req, payload, done) =>
+  done(null),
+)
+
+app.listen({ port: 4000 })
