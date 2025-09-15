c9c943214025dcb0b6ad5061e6501c528924fa22
Josh Girvin <josh@jgirvin.com>
2025-09-15 17:00:10 +1000

    ESLint, Prettier, Yarn SDK handling for PnP

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,2 +1,4 @@
-const str: string = "Hello, world, from Typescript land!\n"
-console.log(str)
+import fastify, { FastifyReply, FastifyRequest } from 'fastify'
+import { createYoga } from 'graphql-yoga'
+
+const app = fastify({ logger: true })
